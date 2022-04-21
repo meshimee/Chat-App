6b2de40e30d97bb70a080e119ed6935200bc2f02
Shimee Aktar Laskar <similaskar1999@gmail.com>
2022-04-22 01:35:03 +0530

    first commit

---
+++ .history/server_20220412024942.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+const http = require('http').createServer(app)
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT, () => {
+          console.log(`Listening on Port ${PORT}`)
+})
+
+app.get('/',(req,res) => {
+          res.sendFile(__dirname + 'public/index.html')
+})
